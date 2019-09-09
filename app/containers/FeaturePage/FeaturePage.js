9897bda2e1a8613a7f3db389c5ec95fad4739f14
MSW-yar <apnashery96@yahoo.com>
2019-09-09 15:35:53 +0500

    changes

--- app/containers/FeaturePage/FeaturePage.js
+++ app/containers/FeaturePage/FeaturePage.js
@@ -6,19 +6,18 @@
 import React from 'react';
 import './style.scss';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class FeaturePage extends React.Component {
-  // eslint-disable-line react/prefer-stateless-function
-
-  // Since state and props are static,
-  // there's no need to re-render this component
-  shouldComponentUpdate() {
-    return false;
-  }
-
   render() {
     return (
       <div className="feature-page">
-        
+        <div className="indi-box">
+          <h3> 8-bit Computer </h3>
+          <h4> Description: Ben Eaters fully functional 8 bit computer with 7 basic components showing the transmission of data via bus and calculations shown by ALU.2 </h4>
+          <div className="numberOfPeople"><p> 4 </p></div>
+          <div className="cost"> 10,000 </div>
+          <div className="last-modification"> last modified <br /> 9-09-19 12: 18</div>
+        </div>
       </div>
     );
   }
